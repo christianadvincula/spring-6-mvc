fff4381ed1cf12d73c7b8e10aaf3b6d129031d0f
christianadvincula <christianadvincula@gmail.com>
2024-09-08 23:05:15 +0800

    added Lombok AllArgConstructor

---
+++ src/main/java/guru/springframework/spring6mvc/controller/BeerController.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring6mvc.controller;
+
+import guru.springframework.spring6mvc.services.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@AllArgsConstructor
+@Controller
+public class BeerController {
+    private final BeerService beerService;
+}
