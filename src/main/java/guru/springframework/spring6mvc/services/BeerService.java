1b0683eac552b45507050fec25a2f6868511aecc
christianadvincula <christianadvincula@gmail.com>
2024-09-08 23:07:15 +0800

    added stereotype annotation Service

--- src/main/java/guru/springframework/spring6mvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6mvc/services/BeerService.java
@@ -1,9 +1,11 @@
 package guru.springframework.spring6mvc.services;
 
 import guru.springframework.spring6mvc.model.BeerData;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public interface BeerService {
     public BeerData getBeerById(UUID id);
 }
