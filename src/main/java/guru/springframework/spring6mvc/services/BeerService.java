398bbc42f24afd6722b90e8df0c4049664b7c27d
christianadvincula <christianadvincula@gmail.com>
2024-09-08 23:11:01 +0800

    put the stereotype annotation Service to ServiceImpl

--- src/main/java/guru/springframework/spring6mvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6mvc/services/BeerService.java
@@ -4,7 +4,7 @@ import guru.springframework.spring6mvc.model.BeerData;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-@Service
+
 public interface BeerService {
     public BeerData getBeerById(UUID id);
 }
